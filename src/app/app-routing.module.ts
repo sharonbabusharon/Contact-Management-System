3e6472862b83a03b7fa939d491bbb3a933cf8dc1
Sharon Babu <sharonbabu107@gmail.com>
2023-03-11 16:16:32 +0530

    added create contact page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { ContactManagerComponent } from './contact-manager/contact-manager.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { UpdateContactComponent } from './update-contact/update-contact.component';
+import { ViewContactComponent } from './view-contact/view-contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+    {
+      path:"",redirectTo:"contact/admin",pathMatch:"full"
+  },
+  {
+    path:"contact/admin",component:ContactManagerComponent
+  },
+  {
+    path:"contact/add",component:AddContactComponent
+  },
+  {
+    path:"contact/update/:Id",component:UpdateContactComponent
+  },
+  {
+    path:"contact/view/:contactId",component:ViewContactComponent
+  },
+  {
+    path:"**",component:PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactManagerComponent } from './contact-manager/contact-manager.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { UpdateContactComponent } from './update-contact/update-contact.component';
+import { ViewContactComponent } from './view-contact/view-contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactManagerComponent,
+    AddContactComponent,
+    UpdateContactComponent,
+    ViewContactComponent,
+    PageNotFoundComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
